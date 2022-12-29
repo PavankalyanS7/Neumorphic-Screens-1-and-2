f04c1de21c2090b24c09e2013c0e468930dda89f
pavankalyangoli <114235142+pavankalyangoli@users.noreply.github.com>
2022-12-29 12:23:42 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,247 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+String _selectedOption = "School";
+String selectedGender = "Male";
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: 680,
+              width: 500,
+              decoration: const BoxDecoration(
+                shape: BoxShape.rectangle,
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFE0E6ED),
+                    Color(0xFFD3D8E0),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(20, 80, 20, 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Center(
+                      child: Icon(Icons.flash_on,
+                          size: 80, color: Colors.blueAccent),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Form(
+                      child: Neumorphic(
+                        style: NeumorphicStyle(
+                          color: Colors.white,
+                          boxShape: NeumorphicBoxShape.roundRect(
+                            BorderRadius.circular(20),
+                          ),
+                        ),
+                        child: const TextFeilds(
+                            name: "Your name", color: Colors.blueAccent),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Neumorphic(
+                      style: NeumorphicStyle(
+                        shape: NeumorphicShape.concave,
+                        boxShape: NeumorphicBoxShape.roundRect(
+                            BorderRadius.circular(20)),
+                        depth: -5,
+                        color: Colors.grey[200],
+                      ),
+                      child: const TextFeilds(
+                          name: "Your e-mail", color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Neumorphic(
+                      style: NeumorphicStyle(
+                        shape: NeumorphicShape.concave,
+                        boxShape: NeumorphicBoxShape.roundRect(
+                            BorderRadius.circular(20)),
+                        depth: -5,
+                        color: Colors.grey[200],
+                      ),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 20,
+                              ),
+                              value: _selectedOption,
+                              items: const [
+                                'School',
+                                'Collage',
+                              ]
+                                  .map((option) => DropdownMenuItem(
+                                        value: option,
+                                        child: Text(option),
+                                      ))
+                                  .toList(),
+                              onChanged: (newValue) {
+                                setState(() {
+                                  _selectedOption = newValue!;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Row(
+                          children: [
+                            Neumorphic(
+                              style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(100)),
+                                depth: -5,
+                              ),
+                              child: Radio(
+                                value: 'Male',
+                                groupValue: selectedGender,
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedGender = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Text(
+                              'Male',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 20),
+                        Neumorphic(
+                          style: NeumorphicStyle(
+                            shape: NeumorphicShape.concave,
+                            boxShape: NeumorphicBoxShape.roundRect(
+                                BorderRadius.circular(100)),
+                            depth: -5,
+                          ),
+                          child: Radio(
+                            value: 'female',
+                            groupValue: selectedGender,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedGender = value!;
+                              });
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Text(
+                          'Female',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Neumorphic(
+                      style: NeumorphicStyle(
+                        shape: NeumorphicShape.concave,
+                        boxShape: NeumorphicBoxShape.roundRect(
+                            BorderRadius.circular(20)),
+                        depth: -5,
+                        lightSource: LightSource.topLeft,
+                        color: Colors.grey[200],
+                      ),
+                      child: const TextFeilds(
+                          name: "Password", color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: NeumorphicButton(
+                        child: const Text(
+                          "Signup",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextFeilds extends StatelessWidget {
+  final Color color;
+  final String name;
+
+  const TextFeilds({super.key, required this.color, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: name,
+        hintStyle: TextStyle(
+          color: color,
+          fontSize: 20,
+        ),
+        contentPadding: const EdgeInsets.all(
+          20,
+        ),
+      ),
+    );
+  }
+}
